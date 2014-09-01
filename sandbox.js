a36a470248dc0a8516d834c3c0250ffe0777bf82
Worry Darque <worry.darque@gmail.com>
2014-09-01 23:51:52 +0200

    en- and decryption implemented. stable state. new version number. we may need unit tests

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,15 @@
 var SpiceFS = require("./lib");
 
+
 SpiceFS.Repository.open("./test3", function(err, repo){
+
     console.log(repo, repo.isFresh);
-	console.log(repo.racks.getPublic());
+    console.log(repo.racks.getPublic());
 	repo.racks.getPublic().foo = "bar";
+    var wortex17 = repo.racks.getPrivate('wortex17', ['abcdefgh']);
+    console.log("#", wortex17);
+    if(wortex17 != null)
+        wortex17.wohoo = "yeeha";
     if(repo && repo.isFresh)
     {
         repo.mkdirpSync();
@@ -17,6 +23,7 @@ SpiceFS.Repository.open("./test3", function(err, repo){
         repo.removeFromFS(function(err){
             if(err)
                 console.warn("Could not remove repo;", err);
+            else console.log("Removed");
         });
     }
 });
