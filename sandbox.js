e522724d01daa3b6cdc5b900d87613118b9927d1
Worry Darque <worry.darque@gmail.com>
2014-11-24 08:12:23 +0100

    INDEV

--- sandbox.js
+++ sandbox.js
@@ -1,25 +1,41 @@
+var lib = require("./lib/v01");
+var fs = require("fs");
 
-var SpicesDrawer = require("./lib/v1/Spices/Drawer");
-
-var drawerA = new SpicesDrawer();
-var drawerB = new SpicesDrawer();
-
-drawerB.Contents["number"] = 500;
-drawerB.recalculateLog();
-
-drawerA.Contents["number"] = 404;
-drawerA.Contents["string"] = 'older';
-drawerA.Contents["object"] = {foo:'bar'};
-drawerA.Contents["array"] = ['cookie'];
-drawerA.Contents["null"] = null;
-drawerA.Contents["object"].ssss = 4;
-
-drawerB.Contents["string"] = 'newer';
-
-drawerA.mergeDrawer(drawerB);
+var Repository = require('./lib/v01/Repository');
+var Spicerack = require('./lib/v01/Spicerack');
 
 
-console.log(drawerA.Log);
-console.log(drawerB.Log);
+//var dir = "\\\\tplinklogin.net\\volume9\\";
+var dir = "testdir";
+
+
+Repository.cleanupAtResource(dir, function(err, repo){
+
+});
+
+/*
+Repository.createAtResource(dir, function(err, repo){
+    Spicerack.getAtResource(dir, ["public", "public"], function(errors, racks, repository){
+
+        var rack = racks["public"];
+        var test = rack.getSpice("test");
+
+        console.log(test.Contents);
+        test.Contents.Alpha = "0mega" + Date.now();
+        console.log(test.Contents);
+
+        rack.storeSpice(test);
+
+
+        rack.storeAtResource(dir, function(err, rack, repo){
+            console.log(err, rack, repo);
+        });
+    });
+});
+
+/**
+Repository.createAtResource("testdir", function(err, repo){
+    console.log(err, repo);
+});
 
 /**/
